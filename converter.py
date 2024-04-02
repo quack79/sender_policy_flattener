f8a2edc98ec65069bb2e96ee379bae11b71ae38f
quack79 <alex@quack.me>
2024-04-02 12:12:41 +0100

    A little progress?

--- converter.py
+++ converter.py
@@ -2,22 +2,27 @@
 import json
 
 # Open JSON file
-f = open('result.json')
- 
-# Return JSON object
-data = json.load(f)
- 
-# Iterating through JSON
-count = 0
-domain = "myclubgroup.co.uk"
+with open("output.json", "r") as f:
 
-for i in data["myclubgroup.co.uk"]['records']:
+    # Return JSON object
+    data = json.load(f)
 
-    if count == 0: print('spf' + '.' + str(domain))
-    else: print('spf{0}'.format(count) + '.' + str(domain))
+    for x in data:
+            domain = x
 
-    print(i)
-    count += 1
- 
-# Close JSON file
-f.close()
+    # Iterating through JSON
+    count = 0
+    #domain = "myclubgroup.co.uk"
+
+    for i in data[domain]['records']:
+        if count == 0: 
+             #print('spf' + '.' + str(domain))
+            start_dns = ('spf' + '.' + str(domain))
+        else: 
+             #print('spf{0}'.format(count) + '.' + str(domain))
+             start_dns = ('spf{0}'.format(count) + '.' + str(domain))
+
+        #print(i)
+        cf_command = "cloudflare-dns " + start_dns + ' "' + i + '"'
+        print(cf_command)
+        count += 1

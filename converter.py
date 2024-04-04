4b63140f0fca5dee093007c9ac6d459f825a90c1
quack79 <alex@quack.me>
2024-04-04 13:35:12 +0100

    Update converter.py
    It actually works!
    
    Definitely not the best code in the world, but it works for my purposes!!

--- converter.py
+++ converter.py
@@ -2,36 +2,31 @@
 import json
 import subprocess
 
-# Open JSON file
+# requires: pip install cloudflare-dns
+# this script must only be run after spflat!
+
+# get API token from CloudFlare, then 
+# set CF_API_TOKEN in environment
+
 with open("output.json", "r") as f:
 
-    # Return JSON object
     data = json.load(f)
 
     for x in data:
             domain = x
 
-    # Iterating through JSON
     count = 0
-    #domain = "myclubgroup.co.uk"
 
     for dns_content in data[domain]['records']:
         if count == 0: 
-             #print('spf' + '.' + str(domain))
-            dns_name = ('spftest' + '.' + str(domain))
+            dns_name = ('spf' + '.' + str(domain))
         else: 
-             #print('spf{0}'.format(count) + '.' + str(domain))
-             dns_name = ('spftest{0}'.format(count) + '.' + str(domain))
+             dns_name = ('spf{0}'.format(count) + '.' + str(domain))
 
-        #print(i)
-        cf_command = ' -sr TXT ' + dns_name + ' "' + dns_content + '" 300'
+        cf_command = 'cloudflare-dns -sr TXT ' + dns_name + ' "' + dns_content + '" 300'
         print(cf_command)
 
-        #result = subprocess.run(["cloudflare-dns", cf_command])
+        result = subprocess.run(cf_command)
         #print(result.stdout)
 
         count += 1
-
-# pip install cloudflare-dns
-# cloudflare-dns -sr TXT spftest.myclubgroup.co.uk "v=spf1 include:_spf.google.com -all"
-
